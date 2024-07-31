1102fd1b346b3279ee108730f0abd9d5951aa10d
Jennifer Saquic <jenntecum2001@gmail.com>
2024-07-31 09:58:40 -0600

    busqueda de recetas

--- Recetas/Recetas/Class1.cs
+++ Recetas/Recetas/Class1.cs
@@ -6,7 +6,69 @@ using System.Threading.Tasks;
 
 namespace Recetas
 {
-    internal class Class1
+    public  class Receta
     {
+        List<string> Nombre {  get; set; }
+        List<string> Ingredientes { get; set; }
+        List< string> Descripcion { get; set; }
+        List<double> Tiempo{ get; set; }
+
+        public Receta(List<string> nombre, List<string> ingredientes, List<string> descripcion, List<double> tiempo)
+        {
+            Nombre = nombre;
+            Ingredientes = ingredientes;
+            Descripcion  = descripcion;
+            Tiempo = tiempo;
+        }
+
+        public Receta() { }
+
+         public void BuscarProducto(string nombre)
+
+        {
+            int posiscion = 0;
+            foreach (string poducto in Nombre)
+            {
+                if (poducto == nombre)
+                {
+                    for (int i = 0; i < Nombre.Count; i++)
+                    {
+                        if (Nombre[i] == nombre)
+                        {
+                            posiscion = i; 
+                            
+                            break;
+                        }
+
+                    }
+                    MostrarProducto(posiscion);
+                }
+               
+            }
+
+            
+           
+        }
+
+        public void MostrarProducto(int posicion)
+        {
+            Console.WriteLine("Nombre: " + Nombre[posicion]);
+            Console.WriteLine("Ingredientes:" +Ingredientes[posicion]);
+            Console.WriteLine("Descripcion: " + Descripcion[posicion]);
+            Console.WriteLine("Tiempo de preparacion"+ Tiempo[posicion]);
+
+        }
+
+        public void MostrarRecetas()
+        {
+            Console.WriteLine("Recetas Disponibles");
+            foreach(string receta in Nombre)
+            { 
+                Console.WriteLine(receta);
+                
+            }
+        }
+
+
     }
 }
--- Recetas/Recetas/Program.cs
+++ Recetas/Recetas/Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Recetas;
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+
+        bool continuar = true;
+        int opcion =0;
+        List<double> tiempo = new List<double>();
+        List<string> nombreReceta = new List<string>();
+        List<string> ingrediente = new List<string>();
+        List<string> descripcion = new List<string>();
+        Receta receta = new Receta();
+
+
+
+
+        do
+        {
+            try
+            {
+                Console.WriteLine(" Recetas: ");
+                Console.WriteLine("1. Ingresar una receta");
+                Console.WriteLine("2. buscar receta ");
+                Console.WriteLine("3. mostrar recetas");
+            
+
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+                while (opcion < 1 || opcion > 4)
+                {
+                    Console.WriteLine("Valor no esta en el rango...")
+                        ; Console.WriteLine("Ingrese un valor: ");
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                }
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear();
+
+                        Console.Write("ingrese el nombre de la receta: ");
+                        string nombre = Console.ReadLine().ToLower();
+                        Console.Write("ingrese los ingredientes: ");
+                        string ingredientesReceta = Console.ReadLine().ToLower();
+                        Console.Write("ingrese la descripcion: ");
+                        string descripcionReceta = Console.ReadLine().ToLower();
+                        Console.Write("ingrese el tiempo de coccion: ");
+                        double coccion = Convert.ToDouble(Console.ReadLine());
+
+                        tiempo.Add(coccion);
+                        nombreReceta.Add(nombre);
+                        descripcion.Add(descripcionReceta);
+                        ingrediente.Add(ingredientesReceta);
+                        receta = new Receta(nombreReceta,ingrediente,descripcion,tiempo);
+                     
+
+                        break;
+                    case 2:
+
+                        Console.Write("ingrese el nombre de la receta que busca : ");
+                        string busquedaReceta = Console.ReadLine().ToLower();
+                        receta.BuscarProducto(busquedaReceta);
+                      
+                        
+                        break;
+                    case 3:
+                        Console.Clear();
+                        receta.MostrarRecetas();
+                        break;
+                    case 4:
+
+                        continuar = false;
+
+                        break;
+
+                    
+
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ha ocurrido un error... " + ex.Message);
+
+            }
+
+
+        } while (continuar == true);
+
+
+    }
+}
